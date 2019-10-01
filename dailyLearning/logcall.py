914624be9b51827c9261e489265079b1d39cdb7a
Shonal Machado <m.shonal@gmail.com>
2019-09-30 21:58:40 -0700

    Added wrapper for doc

--- dailyLearning/logcall.py
+++ dailyLearning/logcall.py
@@ -1,8 +1,10 @@
+from functools import wraps
 
 def logged(function):
 
     # Add logging around the function
 
+    @wraps(function)
     def wrapper(*args, **kwargs):
         print("Calling function: {}".format(function.__name__))
         return function(*args, **kwargs)
